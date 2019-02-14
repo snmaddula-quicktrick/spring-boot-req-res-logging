5f3ebc9cbfd5cb387a72f0d3b98e286af775afbb
snmaddula <snmaddula@outlook.com>
2019-02-14 14:44:59 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/logging/poc/BootReqResLoggingApplication.java
@@ -0,0 +1,14 @@
+package snmaddula.logging.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootReqResLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootReqResLoggingApplication.class, args);
+	}
+
+}
+
